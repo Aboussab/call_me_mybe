11e6abc54c572ae283e558649a3517017a4f7e0a
aboussab <aminebossaboune@gmail.com>
2026-09-04 16:20:09 +0100

    new start

---
+++ parsing.py
@@ -0,0 +1,37 @@
+import json
+import sys
+
+
+class Parsing:
+    def __init__(self, args):
+        "docstring:"
+        for i, arg in enumerate(args):
+            if arg == "--input":
+                self.promts_file = args[i + 1]
+            elif arg == "--functions_definition":
+                self.functions_def_file = args[i + 1]
+            elif arg == "--output":
+                self.output_file = args[i + 1]
+            else:
+                print("UNKNOW_FLAG: pleas make sure to give the right flags")
+                sys.exit(0)
+
+    def loading(file):
+        "docstring"
+        try:
+            with open(file) as f:
+                data = json.load(f)
+        except FileNotFoundError:
+            print("there is an error while opening the fill")
+        except json.JSONDecodeError:
+            print("you should pass a file with the json format")
+        # unpack
+        if isinstance(data, list) and all(isinstance(items,dict) for items in data):
+            print("pleas make sure the data passed is correct")
+        return data
+
+    def parse_functions_definition(path):
+        pass
+
+    def parse_prompts(path):
+        pass
