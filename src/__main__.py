5dcbae1eb80eaa8b788fc6def91350ebfa2611b8
amine-XB8999999999999 <aminebossaboune@gmail.com>
2026-09-05 17:42:14 +0100

    workin on the parsing class

--- src/__main__.py
+++ src/__main__.py
@@ -1,34 +1,48 @@
 from modules import FunctionsDefinition
+from typing import Any
+from pydantic import ValidationError
 from sys import exit
 import json
 
 
-def loading(file):
-    "docstring"
+def loading(file: str) -> list[Any]:
+    """
+    loading is a function that take the file path to be loeaand return it
+    as list of dict.
+    """
     try:
         with open(file) as f:
             data = json.load(f)
     except FileNotFoundError:
         print("there is an error while opening the fill")
-        exit(0)
+        exit(1)
     except json.JSONDecodeError:
         print("you should pass a file with the json format")
-        exit(0)
-    if isinstance(data, list) and all(isinstance(n, dict) for n in data):
-        print("pleas make sure the data passed is correct")
+        exit(1)
+    if not (isinstance(data, list) and all(isinstance(n, dict) for n in data)):
+        print("ERROR DETCTED: Pleas make sure the data passed is correct")
+        exit(1)
     return data
 
 
-def main():
-    file = loading("./functions_definition.json")
+def validation():
+    file = loading("./data/input/functions_definition.json")
 
+    file_as_list = []
     for entry in file:
-        parsed = FunctionsDefinition(**entry)
-        print(parsed.name, "→ OK")
+        try:
+            parsed = FunctionsDefinition(**entry)
+            file_as_list.append(parsed)
+        except ValidationError:
+            print("ERROR DETCTED: Pleas make sure that the data in\
+your json file is on the correct format.")
+            exit(1)
+    return
 
 
 if __name__ == "__main__":
     try:
-        main()
+        validation()
     except Exception as e:
         print(e)
+        exit(1)
--- src/parsing.py
+++ src/parsing.py
@@ -1,27 +1,61 @@
+from modules import FunctionsDefinition
+from typing import Any
+from pydantic import ValidationError
+from sys import exit
 import json
-import sys
 
 
-class Parsing:
-    # def __init__(self, args):
-    #    "docstring:"
-    #    for i, arg in enumerate(args):
-    #        if arg == "--input":
-    #            self.promts_file = args[i + 1]
-    #        elif arg == "--functions_definition":
-    #            self.functions_def_file = args[i + 1]
-    #        elif arg == "--output":
-    #            self.output_file = args[i + 1]
+class Parsing():
 
-    def loading(file):
-        "docstring"
+    def __init__(self):
+        self.promts_fi = None
+        self.functions_def_file = None
+        self.output_file = None
+
+    def parser(self, args):
+        """
+        parser is a fct that take the argument passed to the programe
+        and check if every  
+        """
+        for i, arg in enumerate(args):
+            if arg == "--input":
+                self.promts_fi = args[i + 1]
+            elif arg == "--functions_definition":
+                self.functions_def_file = args[i + 1]
+            elif arg == "--output":
+                self.output_file = args[i + 1]
+
+    def loading(file: str) -> list[Any]:
+        """
+        loading is a function that take the file path to be loeaand return it
+        as list of dict.
+        """
         try:
             with open(file) as f:
                 data = json.load(f)
         except FileNotFoundError:
-            print("there is an error while opening the fill")
+            print("ERROR DETCTED: there is an error while opening the fill")
+            exit(1)
         except json.JSONDecodeError:
-            print("you should pass a file with the json format")
-        if isinstance(data, list) and all(isinstance(n, dict) for n in data):
-            print("pleas make sure the data passed is correct")
+            print("ERROR DETCTED: you should pass a file with the json format")
+            exit(1)
+        if not (
+             isinstance(data, list) and all(isinstance(n, dict) for n in data)
+             ):
+            print("ERROR DETCTED: Pleas make sure the data passed is correct")
+            exit(1)
         return data
+
+    def validation(self, path: str) -> list:
+        file = self.loading(path)
+
+        file_as_list = []
+        for entry in file:
+            try:
+                parsed = FunctionsDefinition(**entry)
+                file_as_list.append(parsed)
+            except ValidationError:
+                print("ERROR DETCTED: Pleas make sure that the data in\
+your json file is on the correct format.")
+                exit(1)
+        return
